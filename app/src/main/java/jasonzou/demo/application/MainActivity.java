68a1dc69f3fe76afd417ec76f8d035362236bc49
邹旭 <frank_zouxu@163.com>
2017-08-30 00:57:36 +0800

    no message

--- app/src/main/java/jasonzou/demo/application/MainActivity.java
+++ app/src/main/java/jasonzou/demo/application/MainActivity.java
@@ -1,32 +1,89 @@
 package jasonzou.demo.application;
 
 import android.app.Activity;
-import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.TextView;
+import android.view.animation.Animation;
+import android.view.animation.OvershootInterpolator;
+import android.view.animation.TranslateAnimation;
+import android.widget.Button;
+import android.widget.ImageView;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 
 public class MainActivity extends Activity {
 
-    @BindView(R.id.tv)
-    TextView tv;
-    @BindView(R.id.shadow)
-    View shadow;
 
+    @BindView(R.id.btn)
+    Button btn;
+    @BindView(R.id.iv)
+    ImageView iv;
+
+    float pos = 100;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
-        tv.setShadowLayer(20, 10, 5, Color.parseColor("#FF0000"));
     }
 
 
-    @OnClick(R.id.shadow)
+    /**
+     * 动画移动view并摆放至相应的位置
+     *
+     * @param view               控件
+     * @param xFromDeltaDistance x起始位置的偏移量
+     * @param xToDeltaDistance   x终止位置的偏移量
+     * @param yFromDeltaDistance y起始位置的偏移量
+     * @param yToDeltaDistance   y终止位置的偏移量
+     * @param duration           动画的播放时间
+     * @param delay              延迟播放时间
+     * @param isBack             是否需要返回到开始位置
+     */
+    public  void moveViewWithAnimation(final View view, final float xFromDeltaDistance, final float xToDeltaDistance, final float yFromDeltaDistance, final float yToDeltaDistance, int duration, int delay, final boolean isBack) {
+        //创建位移动画
+        TranslateAnimation ani = new TranslateAnimation(xFromDeltaDistance, xToDeltaDistance, yFromDeltaDistance, yToDeltaDistance);
+        ani.setInterpolator(new OvershootInterpolator());//设置加速器
+        ani.setDuration(duration);//设置动画时间
+        ani.setStartOffset(delay);//设置动画延迟时间
+        ani.setFillAfter(true);
+        //监听动画播放状态
+        ani.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                int deltaX = (int) (xToDeltaDistance - xFromDeltaDistance);
+                int deltaY = (int) (yToDeltaDistance - yFromDeltaDistance);
+                int layoutX = view.getLeft();
+                int layoutY = view.getTop();
+                int tempWidth = view.getWidth();
+                int tempHeight = view.getHeight();
+                view.clearAnimation();
+                if (isBack == false) {
+                    layoutX += deltaX;
+                    layoutY += deltaY;
+                    view.layout(layoutX, layoutY, layoutX + tempWidth, layoutY + tempHeight);
+                } else {
+                    view.layout(layoutX, layoutY, layoutX + tempWidth, layoutY + tempHeight);
+                }
+            }
+        });
+        view.startAnimation(ani);
+    }
+
+    @OnClick(R.id.btn)
     public void onViewClicked() {
+        moveViewWithAnimation(iv,iv.getX(),iv.getX()+pos,iv.getY(),iv.getY(),200,0,true);
+//        iv.setTranslationX(pos);//父布局内
+        pos += 100;
     }
 }
